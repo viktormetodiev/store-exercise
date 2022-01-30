ba47b70b2f86d17b8f47d74a8848116c23dd33e9
viktormetodiev <viktor.metodiev@sumup.com>
2022-01-30 14:07:11 +0200

    Switch to TS

---
+++ hardhat.config.ts
@@ -0,0 +1,21 @@
+import * as dotenv from 'dotenv';
+
+import { HardhatUserConfig, task } from 'hardhat/config';
+import '@nomiclabs/hardhat-waffle';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+
+dotenv.config();
+
+task('accounts', 'Prints the list of accounts', async (_, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  accounts.forEach((account: SignerWithAddress) => {
+    console.log(account);
+  });
+});
+
+const config: HardhatUserConfig = {
+  solidity: '0.8.11',
+};
+
+export default config;
